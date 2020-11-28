f01df178c3e8e8da3558d51d54c09c65239223e2
Terran Whelan <terranwhelan@gmail.com>
2020-11-28 13:31:38 -0500

    node.js works

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,64 @@
-// array of questions for user
-const questions = [
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-];
+// array of questions for user
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "username"
+    },
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "What is this app for?",
+      name: "app"
+    },
+    {
+        type: "input",
+        message: "How do you use your app?",
+        name: "howTo"
+      },
+      {
+        type: "input",
+        message: "How do you install your app?",
+        name: "installation"
+      },
+      {
+        type: "input",
+        message: "What would you like added to your project?",
+        name: "contributions"
+      },
+      {
+        type: "input",
+        message: "Where should people contact you for questions?",
+        name: "email"
+      },
+      
+  ])
+  .then(function(response) {
+
+    if (response.confirm === response.password) {
+      console.log("Success!");
+    }
+    else {
+      console.log("You forgot your password already?!");
+    }
+  });
+// const questions = [
+//     {
+//         name: 'name',
+//         type: 'input',
+//         message: 'What is your name?'
+//     }
+
+// ];
 
 // function to write README file
 function writeToFile(fileName, data) {
